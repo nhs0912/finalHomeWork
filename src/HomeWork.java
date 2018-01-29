11f0bd32e50e17786800d8c0a6bac4e53c0f0f49
nhs09 <nhs0912@nate.com>
2018-01-29 18:18:34 +0900

    사람 생성 미완성

--- src/HomeWork.java
+++ src/HomeWork.java
@@ -1,30 +1,52 @@
+import java.util.Random;
+
 public class HomeWork {
 
     Human[] humans;
+    Random random = new Random();
+    char[] tmpArr;
 
-    String makeName() {
-        return null;
+    char[] makeName() {
+        //random name 생성
+        char[] names = new char[100];
+        for (int i = 0; i < names.length; i++) {
+            int num = 97 + random.nextInt(26);
+            names[i] = (char) num;
+        }
+        return names;
     }
 
-    String addressName() {
-        return null;
+    char[] makeAddressName() {
+        //random address 생성
+
+        char[] names = new char[250];
+        for (int i = 0; i < names.length; i++) {
+            int num = 97 + random.nextInt(26);
+            names[i] = (char) num;
+        }
+        return names;
     }
 
     void makeHuman() {
         //사람 생성, 4기가 넘으려면 6430900명이 필요하다.
 
-
         int humanCnt = 6430900;
         humans = new Human[humanCnt];
         for (int i = 1; i <= humanCnt; i++) {
-//            humans[i] = new Human(i, );
-
+            humans[i] = new Human(i, makeName(), makeAddressName());
+            if (i % 1000000 == 0)
+                System.out.println(i);
 
         }
     }
 
     public void Solve() {
-
+//        tmpArr = makeName();
+//        for (char ch : tmpArr) {
+//            System.out.print(ch);
+//        }
+//        System.out.println();
+        makeHuman();
 
     }
 
--- src/Human.java
+++ src/Human.java
@@ -2,24 +2,51 @@ public class Human {
 
     byte[] ssnArr = new byte[22];//22bytes ssn
     char[] nameArr = new char[100];//100bytes name
-    char[] addressArr = new char[1000];//500 byte address
+    char[] addressArr = new char[250];//500 byte address
 
     int ssn;
-    String name;
-    String address;
 
     Human() {
 
     }
 
-    Human(int ssn, String name, String address) {
+    Human(int ssn, char[] name, char[] address) {
         this.ssn = ssn;
-        this.name = name;
-        this.address = address;
+        this.nameArr = name;
+        this.addressArr = address;
     }
 
+    public byte[] getSsnArr() {
+        return ssnArr;
+    }
 
+    public void setSsnArr(byte[] ssnArr) {
+        this.ssnArr = ssnArr;
+    }
 
+    public char[] getNameArr() {
+        return nameArr;
+    }
+
+    public void setNameArr(char[] nameArr) {
+        this.nameArr = nameArr;
+    }
+
+    public char[] getAddressArr() {
+        return addressArr;
+    }
+
+    public void setAddressArr(char[] addressArr) {
+        this.addressArr = addressArr;
+    }
+
+    public int getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(int ssn) {
+        this.ssn = ssn;
+    }
 
 
 }
